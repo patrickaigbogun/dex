2fe06b6f3dc37f9b950aae02c4a9fade4c42dd98
patrickaigbogun <rikkislayr@gmail.com>
2026-02-13 15:31:50 +0100

    patch the dev script to ensure routes are pregenerated

--- templates/starter/scripts/dev.ts
+++ templates/starter/scripts/dev.ts
@@ -1,5 +1,30 @@
 import { spawnGroup } from '@dex/dev'
 
+async function runGenerate() {
+	const proc = Bun.spawn(
+		[
+			'bunx',
+			'dex-router',
+			'generate',
+			'--pagesDir',
+			'web/pages',
+			'--layoutsDir',
+			'web/layouts',
+			'--outRoutesTs',
+			'core/router/.generated/routes.ts',
+			'--outRoutesJson',
+			'core/router/.generated/manifest.json',
+			'--outLayoutsTs',
+			'core/router/.generated/layouts.ts',
+		],
+		{ stdout: 'inherit', stderr: 'inherit', stdin: 'inherit' }
+	)
+	const code = await proc.exited
+	if (code !== 0) process.exit(code)
+}
+
+await runGenerate()
+
 spawnGroup([
 	{
 		name: 'routes:watch',
