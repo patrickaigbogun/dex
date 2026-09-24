3d58fa1bd40901703511e2701c068955bb77ae0f
patrickaigbogun <rikkislayr@gmail.com>
2026-09-24 21:31:11 +0100

    fix(templates): remove scripts directory from spa template

--- templates/spa/scripts/build-client.ts
+++
@@ -1,4 +0,0 @@
-// @ts-ignore
-import { runBuildClient } from '../core/bootstrap/scripts'
-
-await runBuildClient()
--- templates/spa/scripts/dev.ts
+++
@@ -1,4 +0,0 @@
-// @ts-ignore
-import { runDev } from '../core/bootstrap/scripts'
-
-await runDev()
